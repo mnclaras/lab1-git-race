3ac2e6d97959a70952d1718f9b98a4fe585ae942
siluf <681721@unizar.es>
2017-09-26 18:50:56 +0200

    Update changes

--- src/test/java/es/unizar/webeng/hello/HelloControllerUnitTest.java
+++ src/test/java/es/unizar/webeng/hello/HelloControllerUnitTest.java
@@ -12,16 +12,25 @@ import java.util.HashMap;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-
-@RunWith(SpringRunner.class)			//JUnit will invoke the SpringRunner class to run the tests in that class instead of the runner built into JUnit.
-@WebMvcTest(HelloController.class)		// Using this annotation will disable full auto-configuration and instead apply only configuration relevant to MVC tests.
+/**
+ * JUnit will invoke the SpringRunner class to run the tests in that class instead of the runner built into JUnit.
+ */
+@RunWith(SpringRunner.class)
+/**
+ * Using this annotation will disable full auto-configuration and instead apply only configuration relevant to MVC tests.
+ */
+@WebMvcTest(HelloController.class)
 
 public class HelloControllerUnitTest {
-
-    @Value("${app.message:Hello World}")		// Annotation that indicates a default value expression for the affected argument.
+    /**
+     * Annotation that indicates a default value expression for the affected argument.
+     */
+    @Value("${app.message:Hello World}")
     private String message;
-
-    @Autowired									// Annotation to auto wire bean on the field.
+    /**
+     * Annotation to auto wire bean on the field.
+     */
+    @Autowired	
     private HelloController controller;
 
     /**
