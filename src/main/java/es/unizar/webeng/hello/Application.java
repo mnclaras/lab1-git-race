f72fc6b6d54ecc2f1515d2f88fdaa90bd619d9dc
siluf <681721@unizar.es>
2017-09-26 18:48:51 +0200

    Update changes

--- src/main/java/es/unizar/webeng/hello/Application.java
+++ src/main/java/es/unizar/webeng/hello/Application.java
@@ -9,7 +9,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 public class Application extends SpringBootServletInitializer {
 
    /**
-    *This main method uses SprinApplication.run() to launch the application.
+    * This main method uses {@code SprinApplication.run()} to launch the application.
     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
