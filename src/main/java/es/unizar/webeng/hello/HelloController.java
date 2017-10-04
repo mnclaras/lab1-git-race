05e3557306540fd14fd490f0352c8cd7540b62b4
fiusa8 <dfusteropalacios@gmail.com>
2017-10-04 19:48:56 +0200

    New messages depending of the hour of the day

--- src/main/java/es/unizar/webeng/hello/HelloController.java
+++ src/main/java/es/unizar/webeng/hello/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Map;
 
 /**
@@ -21,12 +23,19 @@ public class HelloController {
      * Message variable is a message to display.
      */
     @Value("${app.message:Hello World}")
-    private String message;	
+    private String messageMorning;
+    @Value("${app.message:Hello World}")
+    private String messageAfternoon;
+    @Value("${app.message:Hello World}")
+    private String messageEvening;
+    @Value("${app.message:Hello World}")
+    private String messageNight;	
 
     /**
      * Returns the logical name of the view that will be rendered when a GET request for "/" comes in.
      * <p>
-     * It populates the model with some welcome information (the current date and time and a simple welcome message)
+     * It populates the model with some welcome information
+     * (the current date and time and a simple welcome message based on the hour of the day)
      * that the view is going to receive.
      * <p>
      * The annotation @GetMapping is used to specify that the method is going to get called only when a GET request
@@ -37,10 +46,28 @@ public class HelloController {
      */
     @GetMapping("/")
     public String welcome(Map<String, Object> model) {
+	Date date = new Date();
 	//Assign in the key "time" a new date.
-        model.put("time", new Date());
-	//Assign in the key "message" the value of the message variable.
-        model.put("message", message);
+        model.put("time", date);
+
+	//Creates a new calendar instance
+	Calendar calendar = GregorianCalendar.getInstance();
+	//Assigns calendar to given date 
+	calendar.setTime(date);   
+	//Gets hour in 24h format
+	int hour = calendar.get(Calendar.HOUR_OF_DAY); 
+
+	//Assign in the key "message" the value of the specific message variable.
+	if (hour>=8 && hour<12){
+		model.put("message", messageMorning);
+	} else if (hour>=12 && hour<18){
+		model.put("message", messageAfternoon);
+	} else if (hour>=18){
+		model.put("message", messageEvening);
+	} else {
+		model.put("message", messageNight);
+	}
+	
         return "welcome";
     }
 }
